4ae13bcac163903d4ea66c96e98212201c0ccb7d
Blakeley Bond <burrick2003@yahoo.com>
2017-10-02 03:17:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, user_char)
-  else turn
+  else turn(board)
   end
   display_board(board)
 end
